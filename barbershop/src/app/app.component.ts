380c55ef2486df7805eab90d5497244b5bdacd76
Leonardo Silva <leonardoferreira032@gmail.com>
2024-03-07 19:04:41 -0300

    Created footer

--- barbershop/src/app/app.component.ts
+++ barbershop/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/page/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { HomeComponent } from './components/page/home/home.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
