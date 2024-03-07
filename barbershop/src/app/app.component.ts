135bd94850507823f5624e91c560e4ea7e5a19d9
Leonardo Silva <leonardoferreira032@gmail.com>
2024-03-07 17:54:05 -0300

    Home page who we are made

--- barbershop/src/app/app.component.ts
+++ barbershop/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/page/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
